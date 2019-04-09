169056cbfccd18084bf0f8dd2099697642eab916
Mílton <mirtu1370@gmail.com>
2019-04-09 21:43:37 +0100

    if_else

---
+++ if.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title></title>
+ </head>
+ <body>
+
+<?php
+if(condition)
+date('F', time(Agosto))
+{
+	echo"Esta calor";
+
+}
+elseif date('F', time(Agosto)){
+	echo "Não está calor";
+}
+?>
